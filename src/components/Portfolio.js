a866d125e6b4bd1dd25b21b68b590e7037f7f80a
MarkHays <mark.hays1991@gmail.com>
2019-05-16 13:47:47 -0700

    project links open new tab

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -16,7 +16,7 @@ export default class Porfolio extends Component {
               return(
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
-                    <a href={`${item.url}`}>
+                    <a href={`${item.url}`} target="_blank" rel="noopener noreferrer">
                       <img src={`${item.imgurl}`} alt={`${item.name}`} className="item-img"/>
                       <div className="overlay">
                         <div className="portfolio-item-meta">
